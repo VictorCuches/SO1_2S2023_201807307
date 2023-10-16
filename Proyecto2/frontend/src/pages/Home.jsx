cc62a04cabd5487f9376feffe30be2967c26e3e9
Belen Orozco <69135983+VictorCuches@users.noreply.github.com>
2023-10-15 22:20:31 -0600

    feat: node y front socket

--- Proyecto2/frontend/src/pages/Home.jsx
+++ Proyecto2/frontend/src/pages/Home.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import socket from '../socket/Socket';
 
 const Home = () => {
+  const [registros, setRegistros] = useState([]);
+
+  // const loadProcessVM = async () => {
+  //   console.log("REACT: loadProcessVM")
+
+  //   try {
+  //     const response = await fetch(`${API_NODE_URL}/infoCpu/${selectVM}`);
+  //     if (!response.ok) {
+  //       throw new Error("No se pudo obtener la respuesta de la API.");
+  //     }
+
+  //     const data = await response.json();
+  //     setProcessVM(data.Procesos);
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // };
+
+  useEffect(() => {   
+    socket.emit("obtener_registros");
+
+    socket.on("registros", (data) => {
+      console.log("Registros recibidos:", data);
+      setRegistros(data)
+    });
+
+  }, []);
+
   return (
     <div>
-      <h1>Victor Cuches</h1>
+      <ul>
+        {registros.map((registro, index) => (
+          <li key={index}>{registro.clave}: {registro.valor}</li>
+        ))}
+      </ul>
+
     </div>
   )
 }
--- Proyecto2/server_node/app.js
+++ Proyecto2/server_node/app.js
@@ -1,18 +1,102 @@
-const express = require('express'); 
+const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const Redis = require('ioredis');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
- 
-app.use(express.json());
+
+const server = http.createServer(app);
+
 app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+// app.use(bodyParser.json());
+
+const { Server } = require('socket.io');
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  }
+})
+
+
+const client = new Redis({
+  host: 'localhost',
+  port: 6379,
+  password: '',
+  db: 0,
+});
 
 app.get('/', (req, res) => {
   res.send('¡Hola, Mundo desde Node!');
 });
 
-//Routes
-app.use(require('./routes/consultas.js'));
 
-app.listen(port, () => {
-  console.log(`El servidor está escuchando en el puerto ${port}`);
+app.get('/obtener_registros', async (req, res) => {
+  try { 
+    const keys = await client.keys('album:*');
+ 
+    if (keys.length === 0) {
+      return res.json([]);  
+    }
+ 
+    const valores = await client.mget(...keys);
+ 
+    const registros = keys.map((clave, index) => ({
+      clave,
+      valor: valores[index],
+    }));
+
+    res.json(registros);
+  } catch (error) {
+    console.error('Error al obtener registros desde Redis', error);
+    res.status(500).json({ error: 'Error al obtener registros desde Redis' });
+  }
+});
+
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado');
+  
+  // Definir una función que obtiene y emite los registros
+  const obtenerYEnviarRegistros = async () => {
+    try {
+      const keys = await client.keys('album:*');
+
+      if (keys.length === 0) {
+        socket.emit('registros', []); // Enviar una respuesta al cliente
+        return;
+      }
+
+      const valores = await client.mget(...keys);
+
+      const registros = keys.map((clave, index) => ({
+        clave,
+        valor: valores[index],
+      }));
+
+      socket.emit('registros', registros); // Enviar una respuesta al cliente
+    } catch (error) {
+      console.error('Error al obtener registros desde Redis', error);
+      socket.emit('error', 'Error al obtener registros desde Redis'); // Enviar un error al cliente
+    }
+  };
+
+  // Ejecutar la función cada segundo con setInterval
+  const interval = setInterval(obtenerYEnviarRegistros, 1000);
+
+  // Manejar la desconexión del cliente
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+    // Limpiar el intervalo cuando el cliente se desconecta
+    clearInterval(interval);
+  });
+});
+
+
+
+server.listen(port, () => {
+  console.log(`Servidor en ejecución en el puerto ${port}`);
 });
