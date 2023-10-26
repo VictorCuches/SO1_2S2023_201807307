e03ab7d488d2b30cb601451e3003628252b3ebf6
Belen Orozco <69135983+VictorCuches@users.noreply.github.com>
2023-10-26 00:25:02 -0600

    feat: pruebas kubernetes

--- Proyecto2/server_py/app.py
+++ Proyecto2/server_py/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import redis
 import json
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=15)
 
 @app.route('/')
