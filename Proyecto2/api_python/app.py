aa892ab673e34d118f1a750370a5eddc90f72c27
Belen Orozco <69135983+VictorCuches@users.noreply.github.com>
2023-10-29 20:52:26 -0600

    update

--- Proyecto2/api_python/app.py
+++ Proyecto2/api_python/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 # Define una ruta que retornará "Hola mundo" cuando se acceda
 @app.route('/')
 def hola_mundo():
-    return 'Hola mundo 201807307'
+    return 'Pruebas cuchau'
 
 # Si ejecutas este script directamente, inicia la aplicación en el puerto 5000 por defecto
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=9000)
