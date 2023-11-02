6e5fc0dd4e78bf9831cdfd2afdea618b5defd81c
Belen Orozco <69135983+VictorCuches@users.noreply.github.com>
2023-11-02 01:15:14 -0600

    fix deploy files

--- Proyecto2/grpc/grpc-client/main.go
+++ Proyecto2/grpc/grpc-client/main.go
@@ -64,7 +64,7 @@ func insertData(c *fiber.Ctx) error {
 }
 
 func sendRedisServer(rank Data) {
-	conn, err := grpc.Dial("grpc-Server:7001", grpc.WithTransportCredentials(insecure.NewCredentials()),
+	conn, err := grpc.Dial("grpc-server:7001", grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithBlock())
 	if err != nil {
 		log.Fatalln(err)
@@ -90,7 +90,7 @@ func sendRedisServer(rank Data) {
 		log.Fatalln(err)
 	}
 
-	fmt.Println("grpc-Server: " + ret.GetInfo())
+	fmt.Println("grpc-server: " + ret.GetInfo())
 }
 
 func sendMysqlServer(rank Data) {
